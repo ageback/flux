846626f4c2e987f3791e47183c998d394d608f4e
Michael Lu <yeskky@gmail.com>
2022-04-05 09:30:24 +0800

    upgrade to AndroidStudio 2021.1.1P2

--- presentation/src/main/java/com/fidloo/flux/presentation/ui/navigation/NavGraph.kt
+++ presentation/src/main/java/com/fidloo/flux/presentation/ui/navigation/NavGraph.kt
@@ -16,12 +16,13 @@
 package com.fidloo.flux.presentation.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltNavGraphViewModel
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.fidloo.flux.presentation.ui.home.HomeScreen
 import com.fidloo.flux.presentation.ui.home.HomeViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
 object MainDestinations {
     const val HOME_ROUTE = "home"
@@ -36,7 +37,7 @@ fun NavGraph(startDestination: String = MainDestinations.HOME_ROUTE) {
         startDestination = startDestination
     ) {
         composable(MainDestinations.HOME_ROUTE) { backStackEntry ->
-            val viewModel = hiltNavGraphViewModel<HomeViewModel>(backStackEntry = backStackEntry)
+            val viewModel = hiltViewModel<HomeViewModel>()
             HomeScreen(viewModel)
         }
     }
